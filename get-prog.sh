0204e2e3f94d20a15c661410fb28eaa7372bc3fe
solaris765 <bassvaulter@gmail.com>
2019-04-11 07:20:33 -0700

    FEAT: Added handler for special cases.
    In this case`slack` on Kubuntu requires XDG_CURRENT_DESKTOP=Unity in order to display the app icon correctly in the system tray.

--- get-prog.sh
+++ get-prog.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 prog="$1"
+launch=$prog
+
+# Special case
+if [ $prog = "slack" ] ; then
+    launch=$(XDG_CURRENT_DESKTOP=Unity env BAMF_DESKTOP_FILE_HINT=/var/lib/snapd/desktop/applications/slack_slack.desktop /snap/bin/slack %U)
+fi
 
 windows="$(xdotool search --class $prog)"
 if [ -z "$windows" ]
 then
-      $prog&exit  
+      $launch&exit  
 else
       for pid in $windows; do
             xdotool windowactivate $pid
       done
 fi
+
